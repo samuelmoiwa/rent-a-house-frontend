761bf401f92a9ba8d7b478a90eafdee410040374
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-21 19:42:55 +0000

    fil linters error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1E90FF",
+        secondary: "#FF6347",
+      },
+    },
   },
   plugins: [],
-}
-
+};
