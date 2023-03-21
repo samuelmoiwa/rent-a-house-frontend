cd3691e9de3593fa0bd33f3839b7f44ac7e58645
unknown <sharonkessio6@gmail.com>
2023-03-21 22:27:24 +0300

    configure tailwindcss

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> hello world </h1>
+      <h1 className="text-3xl font-bold underline"> hello world </h1>
     </div>
   );
 }
