9e3af9e85feb5e522a488d425b3d1a13a5c9f8af
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-22 10:29:19 +0000

    update tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
       colors: {
         primary: '#1E90FF',
         secondary: '#FF6347',
+        'theme-color': '#97BF0F',
+        'button-color': '#97BF0F',
+        'icon-color': '#576D0D',
+        'focus-color': '#81A30D',
+        'icon-hover-color': '#607711',
+        'button-hover-color': '#B9E623',
+        'font-color': '#424242',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        noto: ['Noto Serif TC', 'serif'],
       },
     },
   },
