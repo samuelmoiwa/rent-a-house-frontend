8febbeeec1446916e803a9f41d8780fb9449f65e
ecekpo <ememekpo@yahoo.com>
2023-03-27 16:37:58 +0100

    add a fetchHouseFailed action in houseslice

--- src/redux/house/displayHouseSlice.js
+++ src/redux/house/displayHouseSlice.js
@@ -1,22 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
-    houses: [],
-    isLoading: false,
-    error: null,
-  };
+  houses: [],
+  isLoading: false,
+  error: null,
+};
 
-  const houseSlice = createSlice({
-    name: 'house',
-    initialState,
-    reducers: {
-        fetchHouseStart: (state) => {
-            state.isLoading = true;
-            state.error = null;
-      },
-      fetchHouseSuccess: (state, action) => {
-        state.houses = action.payload;
-        state.isLoading = false;
-        state.error = null;
-      },
+const houseSlice = createSlice({
+  name: "house",
+  initialState,
+  reducers: {
+    fetchHouseStart: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    fetchHouseSuccess: (state, action) => {
+      state.houses = action.payload;
+      state.isLoading = false;
+      state.error = null;
+    },
+    fetchHouseFailed: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
+});
