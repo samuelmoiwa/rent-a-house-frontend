eb67eccfce264ebcaa34d6c59f8a00cb869e5f9e
ecekpo <ememekpo@yahoo.com>
2023-03-27 14:13:57 +0100

    update addfavorite slice

--- src/redux/favorite/addFavoriteSlice.js
+++ src/redux/favorite/addFavoriteSlice.js
@@ -2,31 +2,39 @@ import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const initialState = {
-    favorites: [],
-    loading: false,
-    error: null
-  };
-  addFavoriteStart: (state) => {
-    state.loading = true;
-    state.error = null;
-  },
-  addFavoriteSuccess: (state, action) => {
-    state.loading = false;
-    state.favorites.push(action.payload);
-  },
-  addFavoriteFailure: (state, action) => {
-    state.loading = false;
-    state.error = action.payload;
-  }
-  export const addFavorite = (houseId) => async (dispatch) => {
-    try {
-        dispatch(addFavoriteStart());
-        const response = await axios.post('/favorites', { houseId });
-        dispatch(addFavoriteSuccess(response.data));
-    } catch (error) {
-        dispatch(addFavoriteFailure(error.message));
-      }
-    };
+  favorites: [],
+  loading: false,
+  error: null
+};
 
-    export const { addFavoriteStart, addFavoriteSuccess, addFavoriteFailure } = favoriteSlice.actions;
-    export default favoriteSlice.reducer;
+export const favoriteSlice = createSlice({
+  name: 'favorites',
+  initialState,
+  reducers: {
+    addFavoriteStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    addFavoriteSuccess: (state, action) => {
+      state.loading = false;
+      state.favorites.push(action.payload);
+    },
+    addFavoriteFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    }
+  }
+});
+
+export const addFavorite = (houseId) => async (dispatch) => {
+  try {
+    dispatch(addFavoriteStart());
+    const response = await axios.post('/favorites', { houseId });
+    dispatch(addFavoriteSuccess(response.data));
+  } catch (error) {
+    dispatch(addFavoriteFailure(error.message));
+  }
+};
+
+export const { addFavoriteStart, addFavoriteSuccess, addFavoriteFailure } = favoriteSlice.actions;
+export default favoriteSlice.reducer;
