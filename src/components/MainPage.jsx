5f89662aca834ca38a2fa47f725a4751106003a2
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-28 19:32:11 +0000

    update MainPage.jsx

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchHouses } from '../redux/house/displayHouseSlice';
+import NavBar from './navBar/NavBar';
 
 const MainPage = () => {
   const dispatch = useDispatch();
@@ -15,12 +17,18 @@ const MainPage = () => {
 
   return (
     <div>
+      <NavBar />
       {houses && houses.map((house) => (
         <div key={house.id}>
           <h2>{house.title}</h2>
           <p>{house.description}</p>
           <p>{house.price}</p>
-          {house.image_url && <img src={house.image_url} alt="My Image" />}
+          {house.image_url && (
+          <img
+            src={house.image_url}
+            alt="My Image"
+          />
+          )}
         </div>
       ))}
     </div>
