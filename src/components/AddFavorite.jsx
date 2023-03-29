353038e28da61e9386817670e4357be5397df874
ecekpo <ememekpo@yahoo.com>
2023-03-29 14:41:38 +0100

    add function that handles dispatch add to favorite

--- src/components/AddFavorite.jsx
+++ src/components/AddFavorite.jsx
@@ -2,4 +2,9 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addFavorite } from '../redux/favorite/addFavoriteSlice';
 
+const AddFavorite = ({ houseId }) => {
+    const dispatch = useDispatch();
 
+const handleAddToFavorites = () => {
+    dispatch(addToFavorites(houseId));
+};
