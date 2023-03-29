e9f4c417158e7584124789b7b7a47ea2609203d4
kessio <sharonkessio6@gmail.com>
2023-03-29 08:59:50 +0300

    create fectchuserstart reducer

--- src/redux/house/userHouseSlice.js
+++ src/redux/house/userHouseSlice.js
@@ -1,7 +1,20 @@
 /* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
 const initialState = {
     houses: [],   // empty array to initalize house
     isLoading: false,
     error: null
+};
 
-}
+const userHouseSlice = createSlice({
+    name: 'userHouse',
+    initialState,
+    reducers: {
+        fetchUserHouseStart: (state) => {
+            state.isLoading = true;
+            state.error = null;
+        },
+    },
+})
