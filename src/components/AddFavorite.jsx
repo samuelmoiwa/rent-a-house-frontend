f34ba35bfb3ddde2bff7476517964f10a08e4feb
ecekpo <ememekpo@yahoo.com>
2023-03-29 15:04:57 +0100

    update add favorite component

--- src/components/AddFavorite.jsx
+++ src/components/AddFavorite.jsx
@@ -8,6 +8,7 @@ const AddFavorite = ({ houseId }) => {
   const handleAddFavorites = () => {
     dispatch(addFavorite(houseId));
   };
+
   return (
     <button onClick={handleAddFavorites} className="bg-button-color py-2 px-3">
       Add To Favorites
