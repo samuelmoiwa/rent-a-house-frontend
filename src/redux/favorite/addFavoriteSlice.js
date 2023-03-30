0179a010dda7f62c4f728aa3edc114c79ad92c25
ecekpo <ememekpo@yahoo.com>
2023-03-30 18:17:51 +0100

    update addFavorite slice

--- src/redux/favorite/addFavoriteSlice.js
+++ src/redux/favorite/addFavoriteSlice.js
@@ -27,23 +27,61 @@ export const favoriteSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    fetchFavoritesStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    fetchFavoritesSuccess: (state, action) => {
+      state.loading = false;
+      state.favorites = action.payload;
+    },
+    fetchFavoritesFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const { addFavoriteStart, addFavoriteSuccess, addFavoriteFailure } = favoriteSlice.actions;
+export const {
+  addFavoriteStart,
+  addFavoriteSuccess,
+  addFavoriteFailure,
+  fetchFavoritesStart,
+  fetchFavoritesSuccess,
+  fetchFavoritesFailure,
+} = favoriteSlice.actions;
 
 export const addFavorite = (houseId, houseIdData) => async (dispatch) => {
   try {
     dispatch(addFavoriteStart());
-    const response = await axios.post(`${apiUrl}/${houseId}/favorites`, houseIdData, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: accessToken,
-      },
-    });
+    const response = await axios.post(
+      `${apiUrl}/${houseId}/favorites`,
+      houseIdData,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: accessToken,
+        },
+      }
+    );
     dispatch(addFavoriteSuccess(response.data));
   } catch (error) {
     dispatch(addFavoriteFailure(error.response?.data?.errors || error.message));
+    throw error;
+  }
+};
+
+export const fetchFavorites = () => async (dispatch) => {
+  try {
+    dispatch(fetchFavoritesStart());
+    const response = await axios.get(`${apiUrl}/favorites`, {
+      headers: {
+        Authorization: accessToken,
+      },
+    });
+    dispatch(fetchFavoritesSuccess(response.data));
+  } catch (error) {
+    dispatch(fetchFavoritesFailure(error.response?.data?.errors || error.message));
     throw error;
   }
 };
