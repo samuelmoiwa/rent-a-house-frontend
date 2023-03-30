809b4256026c1c0ad848cfe256e8939b746b9998
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-30 19:44:24 +0000

    update MyHouses.jsx

--- src/components/MyHouses.jsx
+++ src/components/MyHouses.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-alert */
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable react/button-has-type */
@@ -5,6 +6,8 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { getCurrentUser } from '../redux/user/currentUserSlice';
+import { deleteHouse } from '../redux/house/deleteHouseSlice';
+// import { updateHouse } from '../redux/house/updateHouseSlice';
 import NavBar from './navBar/NavBar';
 
 const MyHouses = () => {
@@ -19,9 +22,35 @@ const MyHouses = () => {
   // Check if user property exists before trying to access it
   const user = currentUser && currentUser.currentUser;
 
+  const modal = document.getElementById('myModal');
+  const confirmButton = document.getElementById('confirm-button');
+  const cancelButton = document.getElementById('cancel-button');
+
+  const handleDeleteHouse = (houseId) => {
+    modal.style.display = 'flex';
+    confirmButton.addEventListener('click', () => {
+      dispatch(deleteHouse(houseId));
+      window.location.reload();
+    });
+    cancelButton.addEventListener('click', () => {
+      modal.style.display = 'none';
+    });
+  };
+
   return (
     <>
       <NavBar />
+
+      <div id="myModal" className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50 hidden ">
+        <div className="bg-white rounded-lg p-6">
+          <p className="text-lg">Are you sure you want to delete this house?</p>
+          <div className="flex justify-end mt-4">
+            <button id="confirm-button" className="bg-red-500 text-white py-2 px-4 rounded-lg mr-2">Confirm</button>
+            <button id="cancel-button" className="border-gray-300 border py-2 px-4 rounded-lg">Cancel</button>
+          </div>
+        </div>
+      </div>
+
       <div className="text-center xl:ml-52 p-10">
         {user && (
         <div>
@@ -47,6 +76,7 @@ const MyHouses = () => {
                       className="bg-button-color py-2 px-3 rounded-b-lg
                     hover:bg-button-hover-color focus:outline-none focus:ring-2 focus:ring-offset-2
                     focus:ring-button-color text-white"
+                      onClick={() => handleDeleteHouse(house.id)}
                     >
                       Delete House
                     </button>
