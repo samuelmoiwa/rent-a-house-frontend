4d9a9dbad683eb87dab7de7a161ad6b1c5284c34
kessio <sharonkessio6@gmail.com>
2023-03-30 06:15:48 +0300

    create deletehouse actions

--- src/redux/house/deleteHouseSlice.js
+++ src/redux/house/deleteHouseSlice.js
@@ -10,6 +10,20 @@ const deleteHouseSlice = createSlice({
     name: 'deleteHouse',
     initialState,
     reducers: {
-        
-    }
-})
+        deleteHouseStart: (state) => {
+            state.isLoading = true;
+            state.error = null;
+        },
+        deleteHouseSuccess: (state, action) => {
+            state.deleteHouse = action.payload;
+            state.isLoading = false;
+            state.error = null;
+        },
+        deleteHouseFailed: (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+        },
+    },
+});
+
+export const { deleteHouseStart, deleteHouseSuccess, deleteHouseFailed } = deleteHouseSlice.actions;
