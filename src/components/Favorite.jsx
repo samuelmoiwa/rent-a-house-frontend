b3d6b9a73cdc711105974fb866df69b576126d10
ecekpo <ememekpo@yahoo.com>
2023-03-30 16:01:22 +0100

    create list of favorite function

---
+++ src/components/Favorite.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { getCurrentUser } from '../redux/user/currentUserSlice';
+
+function Favorite() {
+    const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.currentUser);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Favorite
