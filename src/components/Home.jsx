d954bc339daa69c2dc25735d54105cf1f942896e
samuelmoiwa <samuelmoiwa@gmail.com>
2023-03-30 01:15:45 +0000

    add design for Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import LogoutButton from './auth/Logout';
 import MainPage from './MainPage';
 import NavBar from './navBar/NavBar';
 
 const Home = () => (
   <div>
     <NavBar />
-    <LogoutButton />
     <MainPage />
-    
+
   </div>
 );
 
