1328db6a3111a639e158562fa68633217fe6738d
kessio <sharonkessio6@gmail.com>
2023-03-30 06:08:37 +0300

    create slice

---
+++ src/redux/house/deleteHouseSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    deleteHouse: [],
+    isLoading: false,
+    error: null,
+};
+
+const deleteHouseSlice = createSlice({
+    name: 'deleteHouse',
+    initialState,
+    reducers: {
+        
+    }
+})
