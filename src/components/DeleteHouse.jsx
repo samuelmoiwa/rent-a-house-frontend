87cb3258a8994ca9bbd8245b44635552de620061
samuelmoiwa <samuelmoiwa@gmail.com>
2023-04-01 13:05:03 +0000

    update UI

--- src/components/DeleteHouse.jsx
+++ src/components/DeleteHouse.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-alert */
+/* eslint-disable react/button-has-type */
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteHouse } from '../redux/house/deleteHouseSlice';
 
+// eslint-disable-next-line no-unused-vars
 const DeleteHouse = ({ houseId, onSuccess }) => {
   const dispatch = useDispatch();
 
